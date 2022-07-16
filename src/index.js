b90d45b17d5e2375fbcfb1fe9d17cc0bbcf6b39d
alonsoalfa <afaustoalonso@gmail.com>
2022-07-15 19:25:35 -0500

    Added new function in index path

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
     <div className='container'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<App/>}/>
+          
           <Route path='/pokemons/:id' element={<Pokemon/>} />
           <Route path='/pokemon' element={<Poke/>} />
           <Route path='/170892' element={<Info/>} />
