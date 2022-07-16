fa8a85e6bd30fa34a2ed427ed012c59111c36263
alonsoalfa <afaustoalonso@gmail.com>
2022-07-15 19:24:32 -0500

    feat: Added new function in Apppoke.js

--- src/Apppoke.js
+++ src/Apppoke.js
@@ -5,28 +5,6 @@ import { useTranslation } from "react-i18next";
 
 
 
-function Welcome() {
-  const { t, i18n } = useTranslation(['welcome']);
-  function changeToEnglish() {
-    i18n.changeLanguage("en");
-  }
-
-  function changeToSpanish() {
-    i18n.changeLanguage("es");
-  }
-
-  const welcomes = t("title");
-  const info = t("info");
-
-  return (
-    <div className="language">
-      <br></br>
-      <h1 languagewell>{welcomes}</h1>
-      <h1>{info}</h1>
-    </div>
-  );
-}
-
 function Apppoke() {
   return (
     <>
